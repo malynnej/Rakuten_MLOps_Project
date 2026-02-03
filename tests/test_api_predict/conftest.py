ce93e8edfa9febc6bded7b2f93f8528733570b91
malynnej <jenny-lam@gmx.de>
2026-02-03 11:17:22 +0100

    Jla container (#26)
    * based Docker image on `uv:python3.11-bookworm-slim`
    
    * add pytest dependency
    
    * clean up existing test code
    
    * adapted existing tests to pytest
    
    * update Makefile + time output
    
    * new uv lock
    
    * set scope of fixtures (one API request - several tests)
    
    * tests for /predict and /predict/batch endpoints
    
    * start adapting predict API docker
    
    * fix predict API startup (in container)
    
    * adapted predict API tests to new predict API endpoints
    
    * adapted tests to new predict API
    
    * based Dockerfile on uv image
    
    * Added Docker containers for:
    - data
    - train_model_text
    - evaluate_model
    
    Tested functionality and some curl commands.
    
    * Adjusted Logger error and using existing tokenizer for prediction service.
    
    Open:
    - improving tokenizer loading for data (preprocessing) and training service
    
    * Resolved Ruff lint errors
    
    * Formatting double quotes and long lines (ruff)
    
    * Manually linting comment lines >100 (not included in ruff formatting)
    
    * additional manual formatting due to linting warnings
    
    * moving module imports top of file
    
    * final import order changes, i love ruff :)
    
    * reformatted remaining 2 files
    
    * .
    
    ---------
    
    Co-authored-by: ska39 <246989523+ska39@users.noreply.github.com>

--- tests/test_api_predict/conftest.py
+++ tests/test_api_predict/conftest.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
+
 def pytest_report_header(config):
     """Custom header entry for pytest report"""
-    timestr = datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S%z')
+    timestr = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S%z")
     return f"Session start: {timestr}"
