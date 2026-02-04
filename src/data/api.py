1ede9b2b3c66c0e838cb1fe2b853bf4769b5bdbc
ska39 <246989523+ska39@users.noreply.github.com>
2026-02-04 11:53:38 +0100

    set all apis to run on port 8000 (inside their container)

--- src/data/api.py
+++ src/data/api.py
@@ -612,6 +612,6 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.environ.get("PORT", 8001))
+    port = int(os.environ.get("PORT", 8000))
 
     uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
--- src/evaluate_model/api.py
+++ src/evaluate_model/api.py
@@ -310,6 +310,6 @@ async def get_latest_results():
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.environ.get("PORT", 8004))
+    port = int(os.environ.get("PORT", 8000))
 
     uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
