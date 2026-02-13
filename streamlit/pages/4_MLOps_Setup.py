877c99f7e1102f7c393124882831759c2b0af626
ska39 <246989523+ska39@users.noreply.github.com>
2026-02-13 11:05:58 +0100

    Update Streamlit pages 4+5 (#46)
    * re-generated lock files
    
    * streamlit pages update
    
    * update streamlit pages 4+5

--- streamlit/pages/4_MLOps_Setup.py
+++ streamlit/pages/4_MLOps_Setup.py
@@ -15,28 +15,33 @@ st.title("ML Ops Setup")
 st.write("---")
 
 # ==========GitHub Repository And UV Setup==========
-st.subheader("GitHub Repository And UV Setup")
+st.subheader("GitHub Repository and UV Setup")
 with st.expander("**Problem & Setup**", expanded=False):
     col1, col2 = st.columns(2)
 
     with col1:
         st.markdown("**Problem:**")
         st.write("""
-        * code management, storing and sharing
+        * code management, storage and collaboration
         * project reproducibility and package management
         """)
 
         st.markdown("**Approach:**")
         st.markdown("""
-        GitHub repository: Working with main and further branches https://github.com/malynnej/Rakuten_MLOps_Project.git   
-        uv environment: Working with uv package manager as most up-to-date virtual environment approach  
-        * one environment for project
-        * using uv workspaces with own pyproject.toml and pylock.toml
+        * git / GitHub workflow: 
+          * `main` branch
+          * development branches for new features
+          * pull requests (`dev` -> `main`) with code reviews
+          * GitHub issues for task management
+        * `uv` package manager: dependency and virtual environment management
+        * uv workspaces:
+          * several _workspace members_ managed in single environment
+          * each service has own `pyproject.toml` -> `pylock.toml`
         """)
 
         st.markdown("**Challenges:**")
         st.write("""
-        allowing installation of different package versions across workspaces currently not possible with uv
+        * uv workspaces creates dependencies of python packages between services
         """)
 
     with col2:
@@ -53,20 +58,29 @@ with st.expander("**Problem & Setup**", expanded=False):
     with col1:
         st.markdown("**Problem:**")
         st.write("""
-        * offering production-ready REST API for model serving and operations
-        * enabling integration with other systems and user applications
+        Production-ready REST API for model serving and operations
         """)
 
         st.markdown("**Approach:**")
         st.write("""
-        * Microservices: Separate APIs for data, training, evaluation and prediction services
-        * Health checks: /health endpoints for service availability
+        * Microservices: Individual APIs for ML tasks
+          * **DATA** preparation
+          * model **TRAIN**ing
+          * **EVALUATE** trained model
+          * model **PREDICT**ion
+        * Endpoints, e.g.
+          * PREDICT `/predict_text`: simple string input prediction
+          * TRAIN `/train_model`: start traning of ML model
+          * DATA `/preprocess/from-raw`: raw data preprocessing
+          * (ALL) `/health`: check service availability
         * Automatic documentation and validation
+        * Serving with `uvicorn`
         """)
 
         st.markdown("**Challenges:**")
         st.write("""
-        add security in API (currently security with Nginx only)
+        * API endpoint development more complex than, e.g., calling python functions
+        * Security issues exposing unprotected APIs
         """)
 
     with col2:
@@ -86,16 +100,20 @@ with st.expander("**Problem & Setup**", expanded=False):
     with col1:
         st.markdown("**Problem:**")
         st.write("""
-        Running all components of MLOps project in a single environment makes it hard to manage dependencies, scalability, reproducibility, deployment on different systems, etc.
+        Containerization of the different APIs as microservices to
+        * disentagles dependencies
+        * make services portable, scalable, reproducible
         """)
 
         st.markdown("**Approach:**")
         st.markdown("""
-        Containerization of the different tasks as microservices disentagles dependencies, makes services portable, scalable, reproducible.  
+        
           
-        Docker images:
+        Docker images for each API service:
         * Separate environment setup (build stage) from execution of Docker containers
-        * Ideally: Isolate containers on execution (internet access during build stage only)
+        * Isolate containers on execution - internet access during build stage only (ideally)
+        * Pre-defined images for additional deployments: `nignx`, `prometheus`, `grafana`, ...
+
         Docker compose:  
         * Orchestrate all services (Docker containers) with dependencies, health checks
         * Manage shared resources: Mounted volumes, networks
@@ -109,50 +127,50 @@ with st.expander("**Problem & Setup**", expanded=False):
 
     with col2:
         code = """
-        # file: docker-compose.yml
+    # file: docker-compose.yml
 
-        services:
+    services:
         api_predict:
             build:
-            context: src/predict
-            dockerfile: Dockerfile
+                context: src/predict
+                dockerfile: Dockerfile
             deploy:
-            replicas: 1   # increase for load balancing
+                replicas: 1   # increase for load balancing
             container_name: predict_api
             networks: 
-            - api_network
+                - api_network
             volumes:
-            - ./models:/app/models:ro
-            - ./config:/app/config:ro
+                - ./models:/app/models:ro
+                - ./config:/app/config:ro
             healthcheck:
-            test: ["CMD", "curl", "-f", "http://localhost:8000/health"]
-            interval: 5s
-            timeout: 2s
-            retries: 12
+                test: ["CMD", "curl", "-f", "http://localhost:8000/health"]
+                interval: 5s
+                timeout: 2s
+                retries: 12
 
         nginx:
             build:
-            context: deployments/nginx/
-            dockerfile: Dockerfile
+                context: deployments/nginx/
+                dockerfile: Dockerfile
             container_name: nginx_reverse_proxy
             ports:
-            - "8080:8080"
-            - "8443:8443"
+                - "8080:8080"
+                - "8443:8443"
             networks: 
-            - api_network
-            - edge_network
-            - monitoring_network
+                - api_network
+                - edge_network
+                - monitoring_network
             depends_on:
-            api_predict:
-                condition: service_healthy
+                api_predict:
+                    condition: service_healthy
 
         networks:
-        api_network:
-            # internal network not exposed to host
-            internal: true
-        edge_network:
-            # network exposed to host for reverse proxy
-        """
+            api_network:
+                # internal network not exposed to host
+                internal: true
+            edge_network:
+                # network exposed to host for reverse proxy
+    """
         st.code(code, language="python")
 
 st.write("---")
@@ -160,32 +178,41 @@ st.write("---")
 # ==========Data And Model Versioning==========
 st.subheader("Data And Model Versioning With DVC/Dagshub")
 with st.expander("**Problem & Setup**", expanded=False):
-    st.markdown("**Problem:**")
-    st.write("""
-    * track large datasets and models
-    * reproducibility: ensuring use of same data versions
-    * team collaboration: sharing of data and models with remote storage
-    """)
 
-    st.markdown("**Approach:**")
-    st.markdown("""
-    DVC for version control: tracking with .dvc files
-    * raw data
-    * preprocessed .parquet files
-    * model artifacts
-    * label encoder files
-    * evaluation results
-    DagsHub as remote storage: centralized cloud storage for team access  
-    lightweight files to GitHub: Only metadata (.dvc files) committed to repository  
-    """)
+    col1, col2 = st.columns(2)
 
-    st.markdown("**Challenges:**")
-    st.write("""
-    Further addition of MLFlow to Dagshub rises risk of slowing down training
-    """)
+    with col1:
+        st.markdown("**Problem:**")
+        st.write("""
+        * track large datasets and models
+        * reproducibility: ensuring use of same data versions
+        * team collaboration: sharing of data and models with remote storage
+        """)
 
-    image_GF = Image.open('./streamlit/images/Dagshub.png')
-    st.image(image_GF, width=1100)
+        st.markdown("**Approach:**")
+        st.markdown("""
+        DVC for version control: tracking with `.dvc` files
+        * raw data
+        * preprocessed `.parquet` files
+        * model artifacts
+        * label encoder files
+        * evaluation results
+                    
+        DagsHub as remote storage: 
+        * centralized cloud storage for team access  
+        * lightweight files to GitHub: Only metadata (`.dvc` files) in git
+        """)
+
+        st.markdown("**Challenges:**")
+        st.write("""
+        * Powerful platform with many features (data, experiments, models, ...)
+        * Management of user access tokens
+        """)
+
+    with col2:
+        #image_GF = Image.open('./streamlit/images/Dagshub.png')
+        image_GF = Image.open('./streamlit/images/dagshub_dvc.png')
+        st.image(image_GF, width=1100)
 
 st.write("---")
 
@@ -237,19 +264,22 @@ with st.expander("**Problem & Setup**", expanded=False):
     st.markdown("**Approach:**")
     st.markdown("""
     Forwards requests to responsible API services, e.g.  
-    https://nginx:8443/predict/predict_text --> http://predict_api:8000/predict_text  
+    `https://nginx:8443/predict/predict_text` -> `http://predict_api:8000/predict_text`
+    
     Security features:  
-    * First gatekeeper: Manages authentication (implemented: HTTP basic with username+password on each request)
+    * First gatekeeper: Manages authentication
+      * implemented: HTTP basic with username+password on each request
+      * User roles (_user_, _developer_, _admin_) have access to different APIs
     * Encrypted communication: Only HTTPS to "external world", redirect HTTP requests
-    * Allow/block access to endpoints (stub_status only on monitoring network)
+    * Allow/block access to endpoints (`/nginx_status` only on monitoring network)
     * Limit API requests, eg. 20 requests / second (counter DoS attacks)
-    Allows load balancing between several backends (prepared, but currently only one instance of each service)  
+    * Allows load balancing between several backends (prepared, but currently only one instance of each service)  
     """)
 
     st.markdown("**Challenges:**")
     st.write("""
-    * Forwarding of automatically generated API docs (predict_api:8000/docs as nginx:8443/predict/docs), web interfaces prometheus, grafana (https://nginx:8443/grafana)
-    * Generate (self-signed) SSL certificates accepted for different addresses (nginx:8443, localhost:8443) and tools (curl, python requests, web browsers).
+    * Forwarding of automatically generated API docs, web interfaces of prometheus, grafana, e.g. `predict_api:8000/docs` as `nginx:8443/predict/docs`
+    * Generate (self-signed) SSL certificates accepted for different addresses (`nginx`, `localhost`) and tools (curl, python requests, web browsers).
     """)
 
 st.write("---")
@@ -263,28 +293,28 @@ with st.expander("**Problem & Setup**", expanded=False):
     with col1:
         st.markdown("**Problem:**")
         st.write("""
-        Code has bugs, development (unintentionally) breaks previously existing functionality
-        Automated testing: Discover disfunctionalities as soon as possible
+        Discover disfunctionalities during development as soon as possible by automated testing
         """)
 
         st.markdown("**Approach:**")
         st.write("""
         CI workflow with GitHub Actions
         * Code linting and formatting checks with ruff
-        * Triggered on each push (to configured branches) and pull requests (to main)
+        * Triggered on each push and pull requests (`main`)
+                 
         API tests on system startup
         * Implemented with pytest
-        * Running on startup of full system (docker compose up)
-        * Tests run in individual test containers
-        * Send test requests to API containers, verify response (positive+negative)
-        * Logs written to stdout (docker compose logs) and log files (outside of container)
+        * Running from individual Docker containers
+        * On each startup of full system (`docker compose up`)
+        * Test requests to API containers, verify response (positive+negative)
+        * Logs written to stdout (`docker compose logs`) and log files (outside of container)
         """)
 
         st.markdown("**Challenges:**")
         st.write("""
         * "Realistic setting": From where should test requests originate? (host system, edge_network, ...)
         * Balance between test coverage and execution speed
-        * Visibility, readability (+ ideally automated processing) of test results
+        * Test results: Visibility, readability (+ ideally automated processing)
         """)
 
     with col2:
@@ -309,24 +339,25 @@ st.subheader("Monitoring With Prometheus/Grafana")
 with st.expander("**Problem & Setup**", expanded=False):
     st.markdown("**Problem:**")
     st.markdown("""
-    Provide accessible way / overview of the system performance (+ideally model performance)  
-    --> Early indication of bottlenecks, performance issues + help to track down errors.
+    Provide accessible way / overview of the system performance (+ ideally model performance)
+
+    -> Early indication of bottlenecks, performance issues and help to track down errors.
     """)
 
     st.markdown("**Approach:**")
     st.markdown("""
-    APIs provide performance metrics on /metrics endpoints with prometheus_fastapi_instrumentator  
-    nginx provides status / statistics on /nginx_status endpoint  
-    * nginx-prometheus-exporter transforms to Prometheus readable format
-    Prometheus scrapes metrics endpoints, exporters and stores time series  
-    Grafana visualizes the data from Prometheus in dashboards  
-    * Provisioning: Dashboards and data source (Prometheus) pre-configured as JSON files
+    * APIs provide performance metrics on `/metrics` endpoints with `prometheus_fastapi_instrumentator`
+    * nginx provides status on `/nginx_status` endpoint  
+      * `nginx-prometheus-exporter` transforms to Prometheus readable format
+    * Prometheus scrapes metrics endpoints, exporters and stores time series  
+    * Grafana visualizes the data from Prometheus in dashboards  
+      * Provisioning: Dashboards and data sources persistent as JSON files
     """)
 
     st.markdown("**Challenges:**")
     st.markdown("""
-    Involves mighty tools with lots of features (rely on predefined solutions)  
-    Make Grafana dashboards persistent (without storing full database): Provisioning
+    * Involves mighty tools with lots of features (rely on predefined solutions)  
+    * Make Grafana dashboards persistent (without storing full database): Provisioning
     """)
 
     with st.expander("**Dashboards**", expanded=False):
